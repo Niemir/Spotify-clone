d03d808dc4528e9271c0e58a097dcba6672ad954
Marcin <m.niemirski@time4.digital>
2021-12-18 20:18:43 +0100

    Fetching spotify playlist, style center part of app

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,9 +10,48 @@ import {
 } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-function Sidebar() {
+import { useEffect, useState } from "react";
+import useSpotify from "../hooks/useSpotify";
+
+interface Playlist {
+  collaborative: boolean;
+  description: string;
+  href: string;
+  id: string;
+  images: { height: number; url: string; width: number }[];
+  name: string;
+  owner: { display_name: string; href: string; id: string; type: string };
+  primary_color: null;
+  public: boolean;
+  snapshot_id: string;
+  tracks: {
+    href: string;
+    total: number;
+  };
+  type: string;
+  uri: string;
+}
+interface PlaylistBody {
+  body: {
+    items: Playlist[];
+  };
+}
+
+const Sidebar = () => {
+  const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
+  const [playlists, setPlaylists] = useState<Playlist[]>([]);
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data: PlaylistBody) => {
+        console.log(data);
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
   // console.log(session);
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide">
       <div className="space-y-4 ">
@@ -57,28 +96,14 @@ function Sidebar() {
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
 
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        {playlists.map(({ name, id }) => (
+          <p key={id} className="cursor-pointer hover:text-white">
+            {name}
+          </p>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
