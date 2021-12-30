e23abba6e5f34485ba96429cc88ec77404d383e0
Marcin <m.niemirski@time4.digital>
2021-12-30 12:52:40 +0100

    Change spotify logo

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,7 +5,9 @@ import {
   LiteralUnion,
   signIn,
 } from "next-auth/react";
+import Image from "next/image";
 import { FC } from "react";
+import spotifyLogo from "../public/spotify-logo.png";
 interface LoginProps {
   providers: Record<
     LiteralUnion<BuiltInProviderType, string>,
@@ -16,8 +18,9 @@ interface LoginProps {
 const Login: FC<LoginProps> = ({ providers }) => {
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
-      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
-
+      <div className="mb-5 w-52">
+        <Image src={spotifyLogo} alt="Spotify logo" />
+      </div>
       {Object.values(providers).map((provider: ClientSafeProvider) => (
         <div key={provider.name}>
           <button
